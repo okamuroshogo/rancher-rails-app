91aaff95170dec6667129622c876c0f1a4733a9a
okasho <admin@okamu.ro>
2017-02-17 02:03:47 +0900

    rancher-composeの変数を実装

--- rails-mysql/app.sh
+++ rails-mysql/app.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-git clone -b master https://github.com/SMCommunication/mona.git app
-cd app
+git clone -b master https://github.com/SMCommunication/mona.git .
 bundle install 
 rails db:create
 rails db:migrate
