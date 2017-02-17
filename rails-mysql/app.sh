aac180cec70a5673597ce7cf17807e248abcc490
okasho <admin@okamu.ro>
2017-02-18 04:01:57 +0900

    vertion2を作成

--- rails-mysql/app.sh
+++ rails-mysql/app.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-echo $REPO_BRANCH
+git config http.postBuffer 524288000
+git config --global http.postBuffer 524288000
 git clone -b $REPO_BRANCH $RAILS_REPO_URL
 mkdir -p /app/tmp/sockets
 touch /app/tmp/sockets/puma.sock
 chmod -R 777 /app/tmp
-cd app
-bundle install 
-rails db:create
-rails db:migrate
-rails s -b 0.0.0.0
+cd app && bundle install 
+cd app && rails db:create
+cd app && rails db:migrate
+cd app && rails s -b 0.0.0.0
