2759840ea58b7261aa5f905ae3fb5a3b30f3169e
okasho <admin@okamu.ro>
2017-02-18 13:06:19 +0900

    version0を削除

--- rails-mysql/app.sh
+++ rails-mysql/app.sh
@@ -2,11 +2,15 @@
 export GIT_TRACE_PACKET=1
 export GIT_TRACE=1
 export GIT_CURL_VERBOSE=1
-git clone -b $REPO_BRANCH $RAILS_REPO_URL app
+git gc --aggressive
+git repack -a -f -d --window=250 --depth=250
+git config --global http.postBuffer 24288000
+GIT_CURL_VERBOSE=1 git clone -b $REPO_BRANCH $RAILS_REPO_URL app
 mkdir -p /app/tmp/sockets
 touch /app/tmp/sockets/puma.sock
 chmod -R 777 /app/tmp
-cd app && bundle install 
-cd app && rails db:create
-cd app && rails db:migrate
-cd app && rails s -b 0.0.0.0
+cp -f /datadase.yml /app/confing/database.yml
+cd /app && bundle install 
+cd /app && rails db:create
+cd /app && rails db:migrate
+cd /app && rails s -b 0.0.0.0
